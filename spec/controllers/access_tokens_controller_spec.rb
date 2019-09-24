3c0e07a4e54b683a4650ee585f9bf709f874df10
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-09-24 14:05:57 -0700

    completes lesson 22

--- spec/controllers/access_tokens_controller_spec.rb
+++ spec/controllers/access_tokens_controller_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe AccessTokensController, type: :controller do
 
       it "should return 401 status code" do 
         subject
-        expect(response).to have_http_status(401)
+        expect(response).to have_http_status(:unauthorized)
         end
       it "should return proper error body" do
         subject
@@ -40,7 +40,35 @@ RSpec.describe AccessTokensController, type: :controller do
     end
 
     context "when success request" do
-    
+      let(:user_data) do
+        {
+          login: 'user1',
+          url: 'http://example.com/1',
+          avatar_url: 'http://example.com/avatar/1',
+          name: 'My Name 1'
+        }
+      end
+      before do
+        allow_any_instance_of(Octokit::Client).to receive(
+          :exchange_code_for_token
+        )
+          .and_return('validaccesstoken')
+        allow_any_instance_of(Octokit::Client).to receive(:user).and_return(
+          user_data
+        )
+      end
+      subject { post :create, params: { code: "valid_code" } }
+
+      it "should return 201 status code" do
+        subject
+        expect(response).to have_http_status(:created)
+      end
+
+      it "should return proper json" do
+        expect { subject }.to change{ User.count }.by(1)
+        user = User.find_by(login: 'user1')
+        expect(json_data['attributes']).to eq({"token"=> user.access_token.token})
+      end
     end
   end
 end
