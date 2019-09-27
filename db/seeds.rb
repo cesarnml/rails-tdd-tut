8c55d89b40f90af6537183c6a607a1bba681512f
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-09-27 14:03:51 -0700

    type

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Article.create([
-  {
-    title: 'Article title 1',
-    content: 'Article content 1',
-    slug: 'article-title-1'
-    user: user
-  },
-  {
-    title: 'Article title 2',
-    content: 'Article content 2',
-    slug: 'article-title-2'
-    user: user
-  },
-  {
-    title: 'Article title 3',
-    content: 'Article content 3',
-    slug: 'article-title-3'
-    user: user
-  }
-])
+Article.create(
+  [
+    {
+      title: 'Article title 1',
+      content: 'Article content 1',
+      slug: 'article-title-1',
+      user: user
+    },
+    {
+      title: 'Article title 2',
+      content: 'Article content 2',
+      slug: 'article-title-2',
+      user: user
+    },
+    {
+      title: 'Article title 3',
+      content: 'Article content 3',
+      slug: 'article-title-3',
+      user: user
+    }
+  ]
+)
