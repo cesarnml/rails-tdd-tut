30515adc5a8a19406f66303e0e912fd50f6a2e89
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-09-27 14:05:48 -0700

    defines user in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create({ login: 'jsmith', name: 'John Smith', provider: 'github' })
 Article.create(
   [
     {
